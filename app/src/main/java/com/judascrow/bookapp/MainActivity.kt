118af2b2a0cde7684ee6377cb542e24c2327c447
judascrow <judascrow1@gmail.com>
2022-08-03 16:17:21 +0700

    splash and Main Screen

--- app/src/main/java/com/judascrow/bookapp/MainActivity.kt
+++ app/src/main/java/com/judascrow/bookapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.judascrow.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.judascrow.bookapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+
+        }
+
+        binding.skipBtn.setOnClickListener {
+
+        }
     }
 }
